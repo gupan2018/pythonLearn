0063443fa674becc14d52bfe9e8c3e5a76e0b2e8
圆滚滚 <610077670@qq.com>
2017-12-19 23:03:47 +0800

    简化RabbitM消息持久化代码，去除重复注释，保留关键代码

--- 消息队列/RabbitMQ/RabbitMQ消息持久化/producer端.py
+++ 消息队列/RabbitMQ/RabbitMQ消息持久化/producer端.py
@@ -3,28 +3,22 @@
 
 import pika
 
-# 相当于建立一个socket
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('localhost')
 )
 
-# 申明一个管道，在管道里发消息
 channel = connection.channel()
 
-
 # 声明一个queue，队列名为hello，进行持久化
-channel.queue_declare(queue='hello', durable=True)
-
-# 通过管道发送消息
+channel.queue_declare(queue='hello1', durable=True)
 channel.basic_publish(exchange='',
-                      # queue名字
-                      routing_key='hello',
-                      body='Hello World',
+                      routing_key='hello1',
+                      body='Hello Worldvbdsb',
                       # 持久化队列中的消息
                       properties=pika.BasicProperties(
                           delivery_mode=2
-                      ))
+                      )
+                      )
 
 print("[x] sendt 'Hello World'")
-# 不用关闭管道，直接关闭队列即可
 connection.close()
